a8274d4f272be9f83b33ffc775e8ce06ce50d5dc
Braddock20 <braddockikubano@gmail.com>
2026-08-11 08:04:38 +0300

    Update via gpush

---
+++ src/logger.js
@@ -0,0 +1,6 @@
+import pino from "pino";
+export const logger=pino({
+ level:process.env.LOG_LEVEL||"info",
+ redact:{paths:["req.headers.authorization","req.headers.cookie","*.password","TERABOX_PASSWORD"],censor:"[REDACTED]"}
+});
+
